adac6d81f823d0ce3090c2f6d444c8c426751271
Beknazar <110666044+RazanBek@users.noreply.github.com>
2025-05-22 21:06:06 +0600

    st

---
+++ jalilova c++/proba/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double sum=1, multiplication=1, i=1;
+    do {
+        sum+=i;
+        multiplication*=i;
+        i++;
+    } while(i<=10);
+
+    cout<<"Результат (do...while):"<<endl;
+    cout<<"Сумма: "<<sum<<endl;
+    cout<<"Произведение: "<<multiplication<<endl;
+
+    sum=0;
+    multiplication=1;
+
+    for(i=1;i<=10;i++)
+    {
+        sum+=i;
+        multiplication*=i;
+    }
+
+    cout<<"\nРезультат (for):"<<endl;
+    cout<<"Сумма: "<<sum<<endl;
+    cout<<"Произведение: "<<multiplication<<endl;
+
+    sum=0;
+    multiplication=1;
+    i=1;
+    while(i<=10)
+    {
+        multiplication*=i;
+        sum+=i;
+        i++;
+    }
+    cout<<"\nРезультат (while):"<<endl;
+    cout<<"Сумма: "<<sum<<endl;
+    cout<<"Произведение: "<<multiplication<<endl;
+    //Задание 2
+    sum=0.0;
+    for(i=1;i<=100;i++)
+    {
+        sum+=1.0/pow(i,2);
+    }
+    cout<<"\nРезультат (for):"<<endl;
+    cout<<"Сумма: "<<sum<<endl;
+    i=1;
+    sum=0;
+    while(i<=100)
+    {
+        sum+=1.0/pow(i,2);
+        i++;
+    }
+    cout<<"Сумма (while): "<<sum<<endl;
+    i=1;
+    sum=0;
+    do {
+        sum+=1.0/pow(i,2);
+        i++;
+    } while(i<=100);
+    cout<<"Сумма (do...while): "<<sum<<endl;
+    return 0;
+}
